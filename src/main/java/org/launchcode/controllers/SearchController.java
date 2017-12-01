85f018f9a6bf6c8021ab3bb6e80755b25a52b3b3
mona.aghaee <mona.aghaee@gmail.com>
2017-11-30 18:49:26 -0600

    First commit

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -23,5 +23,31 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs;
+            if (searchTerm.equals(null))
+            {
+                //ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+                jobs = JobData.findAll();
+            }
+            else
+            {
+                //ArrayList<HashMap<String, String>> jobs = JobData.findAll(searchTerm);
+                jobs = JobData.findByValue(searchTerm);
+            }
 
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobsSize", jobs.size() + " Result(s)");
+            model.addAttribute("jobs", jobs);
+        } else {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            model.addAttribute("title", "Jobs with " + ListController.columnChoices.get(searchType) + ": " + searchTerm);
+            model.addAttribute("jobsSize", jobs.size() + " Result(s)");
+            model.addAttribute("jobs", jobs);
+        }
+        return "search";
+    }
 }
